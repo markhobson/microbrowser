466139cc45b58b36ad6b8b4f430147e70ab3d4ea
Mark Hobson <markhobson@gmail.com>
2013-11-21 00:23:42 +0000

    Added test for getting <link> itemprop value

--- tck/src/main/java/org/hobsoft/microbrowser/tck/MicrobrowserTck.java
+++ tck/src/main/java/org/hobsoft/microbrowser/tck/MicrobrowserTck.java
@@ -94,6 +94,24 @@ public abstract class MicrobrowserTck
 	}
 
 	@Test
+	public void itemPropertyValueWhenLinkReturnsAbsoluteHref() throws IOException, InterruptedException
+	{
+		server.enqueue(new MockResponse().setBody("<html><body>"
+			+ "<div itemscope='itemscope' itemtype='i'>"
+			+ "<link itemprop='p' href='x'/>"
+			+ "</div>"
+			+ "</body></html>"));
+		server.play();
+		
+		String actual = newBrowser().get(url(server))
+			.getItem("i")
+			.getProperty("p")
+			.getValue();
+		
+		assertThat("item property value", actual, equalToIgnoringCase(server.getUrl("/x").toString()));
+	}
+
+	@Test
 	public void itemPropertyValueWhenUnknownReturnsText() throws IOException, InterruptedException
 	{
 		server.enqueue(new MockResponse().setBody("<html><body>"
