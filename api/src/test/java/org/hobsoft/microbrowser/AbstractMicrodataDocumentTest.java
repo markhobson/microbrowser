1512e9d1739457a024fa4bb15623c22332af1b08
Mark Hobson <markhobson@gmail.com>
2013-12-20 16:35:39 +0000

    Changed AbstractMicrodataDocumentTest to not use Mockito since we need to super constructor calls

--- api/src/test/java/org/hobsoft/microbrowser/AbstractMicrodataDocumentTest.java
+++ api/src/test/java/org/hobsoft/microbrowser/AbstractMicrodataDocumentTest.java
@@ -14,8 +14,8 @@
 package org.hobsoft.microbrowser;
 
 import java.util.Collections;
+import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static java.util.Arrays.asList;
@@ -23,7 +23,6 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Tests {@code AbstractMicrodataDocument}.
@@ -37,24 +36,21 @@ public class AbstractMicrodataDocumentTest
 	private AbstractMicrodataDocument document;
 	
 	// ----------------------------------------------------------------------------------------------------------------
-	// test methods
-	// ----------------------------------------------------------------------------------------------------------------
-
-	@Before
-	public void setUp()
-	{
-		document = mock(AbstractMicrodataDocument.class);
-	}
-
-	// ----------------------------------------------------------------------------------------------------------------
 	// tests
 	// ----------------------------------------------------------------------------------------------------------------
 
 	@Test
 	public void getItemWhenItemReturnsItem()
 	{
-		MicrodataItem item = mock(MicrodataItem.class);
-		when(document.getItems("x")).thenReturn(asList(item));
+		final MicrodataItem item = mock(MicrodataItem.class);
+		document = new FakeMicrodataDocument()
+		{
+			@Override
+			public List<MicrodataItem> getItems(String type)
+			{
+				return "x".equals(type) ? asList(item) : null;
+			}
+		};
 		
 		assertThat(document.getItem("x"), is(item));
 	}
@@ -62,9 +58,16 @@ public class AbstractMicrodataDocumentTest
 	@Test
 	public void getItemWhenItemsReturnsFirstItem()
 	{
-		MicrodataItem item1 = mock(MicrodataItem.class);
-		MicrodataItem item2 = mock(MicrodataItem.class);
-		when(document.getItems("x")).thenReturn(asList(item1, item2));
+		final MicrodataItem item1 = mock(MicrodataItem.class);
+		final MicrodataItem item2 = mock(MicrodataItem.class);
+		document = new FakeMicrodataDocument()
+		{
+			@Override
+			public List<MicrodataItem> getItems(String type)
+			{
+				return "x".equals(type) ? asList(item1, item2) : null;
+			}
+		};
 		
 		assertThat(document.getItem("x"), is(item1));
 	}
@@ -72,7 +75,14 @@ public class AbstractMicrodataDocumentTest
 	@Test(expected = IllegalArgumentException.class)
 	public void getItemWhenNoItemsThrowsException()
 	{
-		when(document.getItems("x")).thenReturn(Collections.<MicrodataItem>emptyList());
+		document = new FakeMicrodataDocument()
+		{
+			@Override
+			public List<MicrodataItem> getItems(String type)
+			{
+				return "x".equals(type) ? Collections.<MicrodataItem>emptyList() : null;
+			}
+		};
 		
 		document.getItem("x");
 	}
---
+++ api/src/test/java/org/hobsoft/microbrowser/FakeMicrodataDocument.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hobsoft.microbrowser;
+
+import java.util.List;
+
+/**
+ * Fake {@code AbstractMicrodataDocument} for testing.
+ */
+public class FakeMicrodataDocument extends AbstractMicrodataDocument
+{
+	// ----------------------------------------------------------------------------------------------------------------
+	// MicrodataDocument methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<MicrodataItem> getItems(String type)
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean hasLink(String rel)
+	{
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Link getLink(String rel)
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getCookie(String name)
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public MicrodataDocument get(String url)
+	{
+		return null;
+	}
+}
