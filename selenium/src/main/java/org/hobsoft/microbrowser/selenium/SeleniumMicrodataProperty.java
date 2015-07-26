336ca22457188924555a27d9444941bc5f1b72fa
Mark Hobson <markhobson@gmail.com>
2015-07-26 19:34:44 +0100

    Corrected block comment

--- selenium/src/main/java/org/hobsoft/microbrowser/selenium/SeleniumMicrodataProperty.java
+++ selenium/src/main/java/org/hobsoft/microbrowser/selenium/SeleniumMicrodataProperty.java
@@ -39,7 +39,7 @@ class SeleniumMicrodataProperty extends AbstractMicrodataProperty
 	}
 
 	// ----------------------------------------------------------------------------------------------------------------
-	// MicrodataItemValue methods
+	// MicrodataProperty methods
 	// ----------------------------------------------------------------------------------------------------------------
 
 	public <T> T unwrap(Class<T> type)
