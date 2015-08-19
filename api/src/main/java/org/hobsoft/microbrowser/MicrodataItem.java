15d97f2bf95e16ed184356a8b4ff944270a2095e
Mark Hobson <markhobson@gmail.com>
2015-08-19 21:27:02 +0100

    Revert "Ensured MicrodataItem.getType returns null when itemtype missing"
    This reverts commit cd0462860be2c665750e7695d7c17ea24234a673.
    
    Deferring until nested property search algorithm implemented as getItemValue
    currently gives misleading results.

--- api/src/main/java/org/hobsoft/microbrowser/MicrodataItem.java
+++ api/src/main/java/org/hobsoft/microbrowser/MicrodataItem.java
@@ -30,7 +30,7 @@ public interface MicrodataItem extends HypermediaContainer
 	/**
 	 * Gets the type of this item.
 	 * 
-	 * @return the item type, or {@code null} if not specified
+	 * @return the item type
 	 */
 	URL getType();
 	
